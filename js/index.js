c0a654eb677e1dc49b12cb84688f723879536574
Vitalii <ridikon666@gmail.com>
2017-06-29 10:51:19 +0000

    Fibonacci

--- js/index.js
+++ js/index.js
@@ -1,14 +1,14 @@
 var fiboNum = Number(prompt('Введіть число'));
 
 function fibonacci(num) {
-    var a = 1,
-        b = 1;
+    var fiboRabbit1 = 1,
+        fiboRabbit2 = 1;
     for (var i = 3; i <= num; i++) {
-        var c = a + b;
-        a = b;
-        b = c;
+        var fiboRabbitSum = fiboRabbit1 + fiboRabbit2;
+        fiboRabbit1 = fiboRabbit2;
+        fiboRabbit2 = fiboRabbitSum;
     }
-    return b;
+    return fiboRabbit2;
 }
 
 document.write(fibonacci(fiboNum));
